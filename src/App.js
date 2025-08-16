bd2990522e6dc42140b18623d7e97269d23aeb76
Carlos <martin5435412@gmail.com>
2025-08-16 11:57:23 -0500

    .

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          x
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <div id = "test">
+        <h1>Probando</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
